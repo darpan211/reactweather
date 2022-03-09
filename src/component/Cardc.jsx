a00ecd844bb623110a8334fb64dd6dae8984630e
Darpan <darpan.bvminfotech@gmail.com>
2022-03-09 11:37:08 +0530

    fix bugs

--- src/component/Cardc.jsx
+++ src/component/Cardc.jsx
@@ -1,41 +1,41 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Card, Button, Form } from "bootstrap-4-react";
 import WbSunnyIcon from "@material-ui/icons/WbSunny";
- 
+
 const title = {
   fontSize: "35px",
   fontWeight: "bold",
   display: "flex",
   alignItems: "baseline",
   justifyContent: "center",
-  textTransform :"capitalize",
-  overflow: 'hidden'
+  textTransform: "capitalize",
+  overflow: "hidden",
 };
 const shape = {
-  position: 'absolute',
-  width: '150px',
-  top: '.5rem',
-  left: '.5rem',
-}
+  position: "absolute",
+  width: "150px",
+  top: ".5rem",
+  left: ".5rem",
+};
 const contentCenter = {
-     height: '100vh',
-    justifyContent: 'center',
-    display: 'flex',
-    alignItems: 'center',
-}
+  height: "100vh",
+  justifyContent: "center",
+  display: "flex",
+  alignItems: "center",
+};
 const cs = {
   width: "25rem",
   border: "0px",
   margin: "25px",
   borderRadius: "10px",
   boxShadow: "0 .5rem 1rem rgba(0,0,0,.15)",
-  background:" rgba(255, 255, 255, .7)",
-	backdropFilter: 'blur(10px)'
+  background: " rgba(255, 255, 255, .7)",
+  backdropFilter: "blur(10px)",
 };
 export default function Cardc() {
   const [city, setCity] = useState(null);
   const [search, setSearch] = useState("Surat");
-  useEffect( () => {
+  useEffect(() => {
     /// ASYNC Await Using API Call
     // const fetchApi = async ()=> {
     //   const res = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=b36d38b366e62b7ce3c598f283b78a2b`);
@@ -46,16 +46,17 @@ export default function Cardc() {
     // fetchApi();
 
     /// by Using fetch() API Call
- 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=b36d38b366e62b7ce3c598f283b78a2b`)
-      .then(res => res.json())
+
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=b36d38b366e62b7ce3c598f283b78a2b`
+    )
+      .then((res) => res.json())
       .then(
         (result) => {
-          console.log(result.main);
           setCity(result.main);
         },
         (error) => {
-          console.log(error);
+          setCity(null);
         }
       );
   }, [search]);
@@ -63,54 +64,54 @@ export default function Cardc() {
   return (
     <>
       <Container style={contentCenter}>
-      <div>
-        <Row>
-          <Col style={{ textAlign: "center" }} col="4" alignSelf="center">
-            <Card display="inline-block" align="top" mr="3" style={cs}>
-              <Card.Header style={{ borderBottom: "0px" }}>
-                <Form.LabelCol htmlFor="search">Search City Here</Form.LabelCol>
-                <Form.Input
-                  type="text"
-                  value={search}
-                  autoComplete='off'
-                  onChange={ (e) => {setSearch(e.target.value)}}
-                  autoFocus
-                  placeholder="Enter City"
-                  id="search"
-                />
-              </Card.Header>
-              <Card.Body>   
-
-              {
-                (!city)  ? (
-                  <Card.Text style={title}>
-                    No Data Found
-                    </Card.Text>
-                ) : (
-                  <>
-                  <Card.Text style={title}>
-                      <WbSunnyIcon /> {search}
-                    </Card.Text>
-                    <Card.Text>
-                      <Button danger outline>
-                        <WbSunnyIcon /> {city.temp} °C
-                      </Button>
-                    </Card.Text>
-                    <Card.Text
-                      style={{
-                        justifyContent: "space-around",
-                        display: "flex",
-                      }}>
-                      <Button primary>Min. Temp : {city.temp_min} °C</Button>
-                      <Button primary>Max. Temp : {city.temp_max} °C</Button>
-                    </Card.Text>
-                  </>
-                )
-              }
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
+        <div>
+          <Row>
+            <Col style={{ textAlign: "center" }} col="4" alignSelf="center">
+              <Card display="inline-block" align="top" mr="3" style={cs}>
+                <Card.Header style={{ borderBottom: "0px" }}>
+                  <Form.LabelCol htmlFor="search">
+                    Search City Here
+                  </Form.LabelCol>
+                  <Form.Input
+                    type="text"
+                    value={search}
+                    autoComplete="off"
+                    onChange={(e) => {
+                      setSearch(e.target.value);
+                    }}
+                    autoFocus
+                    placeholder="Enter City"
+                    id="search"
+                  />
+                </Card.Header>
+                <Card.Body>
+                  {!city ? (
+                    <Card.Text style={title}>No Data Found</Card.Text>
+                  ) : (
+                    <>
+                      <Card.Text style={title}>
+                        <WbSunnyIcon /> {search}
+                      </Card.Text>
+                      <Card.Text>
+                        <Button danger outline>
+                          <WbSunnyIcon /> {city.temp} °C
+                        </Button>
+                      </Card.Text>
+                      <Card.Text
+                        style={{
+                          justifyContent: "space-around",
+                          display: "flex",
+                        }}
+                      >
+                        <Button primary>Min. Temp : {city.temp_min} °C</Button>
+                        <Button primary>Max. Temp : {city.temp_max} °C</Button>
+                      </Card.Text>
+                    </>
+                  )}
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
         </div>
       </Container>
     </>
